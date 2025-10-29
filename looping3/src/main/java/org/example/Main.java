da2a44b1207fdea9c7b6e5794032fdb772ee959d
IgorRRamos <ramosigorprofissional@gmail.com>
2025-10-29 19:40:11 -0300

    New Java class on looping For

---
+++ looping3/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        for (int i = 0; i < 10; i++, counter++) {
+            System.out.println(counter);
+        }
+
+    }
+}
