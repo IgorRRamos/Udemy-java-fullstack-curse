a7c881af9639c43095334766a6e72c8a1e6f5f59
IgorRRamos <ramosigorprofissional@gmail.com>
2025-11-10 19:01:58 -0300

    New java class for each

---
+++ forEach/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] x = {1, 2, 3, 4, 5};
+
+        for (int list : x) {
+            System.out.println(list);
+        }
+    }
+}
