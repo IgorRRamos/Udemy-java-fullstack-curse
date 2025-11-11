d81da343d47f0f04cfec0dc7dd1ad8d5400b0481
IgorRRamos <ramosigorprofissional@gmail.com>
2025-11-11 20:00:38 -0300

    Update

---
+++ JavaMethods/methodWhitReturn/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        int area = triangle(20, 30);
+        System.out.println(area);
+        */
+
+        System.out.println(triangle(20, 30));
+
+    }
+
+    public static int triangle(int heigth, int width) {
+        return heigth * width;
+    }
+}
