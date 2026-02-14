622c328826b4abb15da2e4ed37a739a9848a0608
IgorRRamos <ramosigorprofissional@gmail.com>
2026-02-13 21:38:59 -0300

    Adicionando novas funcionalidades, usando while para continuar jogando.

--- campeonatoBrasileiro/src/main/java/org/example/Main.java
+++ campeonatoBrasileiro/src/main/java/org/example/Main.java
@@ -8,31 +8,57 @@ public class Main {
     public static void main(String[] args) {
         GerenciadorCampeonato brasileiro = new GerenciadorCampeonato();
         Scanner teclado = new Scanner(System.in);
+        int escolha=0;
 
-        brasileiro.adicionar("Atlético-MG     ");
-        brasileiro.adicionar("Bahia           ");
-        brasileiro.adicionar("Botafogo        ");
-        brasileiro.adicionar("Ceará           ");
-        brasileiro.adicionar("Corinthians     ");
-        brasileiro.adicionar("Cruzeiro        ");
-        brasileiro.adicionar("Flamengo        ");
-        brasileiro.adicionar("Fluminense      ");
-        brasileiro.adicionar("Fortaleza       ");
-        brasileiro.adicionar("Grêmio          ");
-        brasileiro.adicionar("Internacional   ");
-        brasileiro.adicionar("Juventude       ");
-        brasileiro.adicionar("Mirassol        ");
-        brasileiro.adicionar("Palmeiras       ");
-        brasileiro.adicionar("Bragantino      ");
-        brasileiro.adicionar("Santos          ");
-        brasileiro.adicionar("São Paulo       ");
-        brasileiro.adicionar("Sport           ");
-        brasileiro.adicionar("Vasco da Gama   ");
-        brasileiro.adicionar("Vitória         ");
 
-        brasileiro.jogarRodada();
-        brasileiro.jogarRodada();
+        do{
 
-        brasileiro.tabela();
+            System.out.println(" ");
+            System.out.println("1 - Adicionar times");
+            System.out.println("2 - Jogar rodada");
+            System.out.println("3 - Mostrar tabela");
+            System.out.print("\n--> ");
+            escolha = teclado.nextInt();
+
+            switch (escolha){
+                case 1:
+                    brasileiro.adicionar("Flamengo            ");
+                    brasileiro.adicionar("Palmeiras           ");
+                    brasileiro.adicionar("São Paulo           ");
+                    brasileiro.adicionar("Corinthians         ");
+                    brasileiro.adicionar("Santos              ");
+                    brasileiro.adicionar("Grêmio              ");
+                    brasileiro.adicionar("Internacional       ");
+                    brasileiro.adicionar("Atlético Mineiro    ");
+                    brasileiro.adicionar("Cruzeiro            ");
+                    brasileiro.adicionar("Fluminense          ");
+                    brasileiro.adicionar("Botafogo            ");
+                    brasileiro.adicionar("Vasco da Gama       ");
+                    brasileiro.adicionar("Athletico Paranaense");
+                    brasileiro.adicionar("Bahia               ");
+                    brasileiro.adicionar("Fortaleza           ");
+                    brasileiro.adicionar("Bragantino          ");
+                    brasileiro.adicionar("Cuiabá              ");
+                    brasileiro.adicionar("Goiás               ");
+                    brasileiro.adicionar("Coritiba            ");
+                    brasileiro.adicionar("América Mineiro     ");
+
+                    System.out.println("20 times tradicionais foram adicionados.");
+                    System.out.println("\n");
+                    break;
+
+                case 2:
+                    brasileiro.jogarRodada();
+                    System.out.println("Fim da primeira rodada!");
+                    break;
+
+                case 3:
+                    brasileiro.tabela();
+                    break;
+
+            }
+
+        } while(true);
+
     }
 }
