f150b46aafa4f315382b2d1b9b72598cc45f7d12
IgorRRamos <ramosigorprofissional@gmail.com>
2026-03-23 22:05:05 -0300

    Aula teorica JAVA basico, tipos primitivos

---
+++ TiposPrimitivos/aula01/src/main/groovy/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+/*
+    temos 8 tiopos de tipos primitivos
+    Todos os tipos primitivos iniviam com letras minusculas.
+    São os tipos que vão guardar em memoria valores simples
+
+ */
+public class Main {
+    public static void main(String[] args) {
+        int num = 10; //Armazena apenas numeros inteiros
+        long num1 = 1029410; //Armazena numeros inteiros, porém aloca mais espaço na memoria.
+        double floatstop = 10.1; //Armazena numeros com ponto flutuante.
+        float otherNumerFloatStop = 125.1f; //Armazena valor de ponto flutuante
+        byte number = 127; //Armazena apenas até no maximo o numero 127 ou -128 tamanho 1 byte
+        short numberShort = 12232; // 2 bytes
+        boolean trueOrFalse = true; //Armazena um valor booleano True or False
+        boolean falseOrTrue = false; //Armazena um valor booleano True or False
+        char letter = 'A'; //Armazena um caracter
+
+
+    }
+}
